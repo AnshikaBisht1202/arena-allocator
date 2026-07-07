ad25395773a1cb640252dcdb604c0fd53ccebc3c
anshieeeka <anshikabisht1202@gmail.com>
2026-07-07 14:59:57 +0000

    Implement arena allocator

---
+++ main.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef s8 b8;
+typedef s32 b32;
+
+#define KiB(n) ((u64)(n) << 10)
+#define MiB(n) ((u64)(n) << 20)
+#define GiB(n) ((u64)(n) << 30)
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define ARENA_BASE_POS (sizeof(mem_arena))
+#define ARENA_ALIGN (sizeof(void*))
+
+// round n up to the next multiple of p, p must be a power of 2
+#define ALIGN_UP_POW2(n, p) (((u64)(n) + ((u64)(p) - 1)) & (~((u64)(p) - 1))) 
+
+typedef struct {
+    u64 reserve_size;
+    u64 commit_size;
+    u64 current_pos;
+    u64 commit_pos;
+} mem_arena;
+
+mem_arena* arena_create(u64 reserve_size, u64 commit_size); 
+void arena_destroy(mem_arena* arena);
+void* arena_push(mem_arena* arena, u64 size, b32 non_zero);
+void arena_pop(mem_arena* arena, u64 size);
+void arena_pop_to(mem_arena* arena, u64 tar_pos);
+void arena_reset(mem_arena* arena);
+
+u32 platform_mem_pagesize();
+void* platform_mem_reserve(u64 size);
+b32 platform_mem_commit(void* ptr, u64 size);
+b32 platform_mem_decommit(void* ptr, u64 size); 
+b32 platform_mem_release(void* ptr, u64 size); 
+
+int main(void) {
+    mem_arena* perm_arena = arena_create(GiB(1), MiB(1));
+
+    if (!perm_arena) {
+        fprintf(stderr, "Failed to create permanent arena\n");
+        return 1;
+    }
+    
+    while (1) {
+        arena_push(perm_arena, MiB(16), false);
+        getc(stdin);
+    }
+
+    arena_destroy(perm_arena);
+
+    return 0;
+}
+
+#if defined(__linux__)
+
+#include <unistd.h>
+#include <sys/mman.h>
+
+u32 platform_mem_pagesize(void) {
+    return (u32)sysconf(_SC_PAGESIZE);
+}
+
+void* platform_mem_reserve(u64 size) {
+    void* out = mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (out == MAP_FAILED) {
+        return NULL;
+    }
+    return out;
+}
+
+b32 platform_mem_commit(void* ptr, u64 size) {
+    s32 ret = mprotect(ptr, size, PROT_READ | PROT_WRITE);
+    return ret == 0;
+}
+
+b32 platform_mem_decommit(void* ptr, u64 size) {
+    s32 ret = mprotect(ptr, size, PROT_NONE);
+    if (ret != 0) return false;
+    ret = madvise(ptr, size, MADV_DONTNEED);
+    return ret == 0;
+}
+
+b32 platform_mem_release(void* ptr, u64 size) {
+    s32 ret = munmap(ptr, size);
+    return ret == 0;
+}
+
+#endif
+
+mem_arena* arena_create(u64 reserve_size, u64 commit_size) {
+    u32 pagesize = platform_mem_pagesize();
+    reserve_size = ALIGN_UP_POW2(reserve_size, pagesize);
+    commit_size = ALIGN_UP_POW2(commit_size, pagesize);
+    if (commit_size > reserve_size) {
+        return NULL;
+    }
+    mem_arena* arena = platform_mem_reserve(reserve_size);
+
+    if (!arena) {
+        return NULL;
+    }
+
+    if (!platform_mem_commit(arena, commit_size)) {
+        platform_mem_release(arena, reserve_size);
+        return NULL;
+    }
+
+    arena->reserve_size = reserve_size;
+    arena->commit_size = commit_size;
+    arena->current_pos = ARENA_BASE_POS;
+    arena->commit_pos = commit_size;
+    
+    return arena;
+}
+
+void arena_destroy(mem_arena* arena) {
+    if (arena) {
+        platform_mem_release(arena, arena->reserve_size);
+    }
+}
+
+void* arena_push(mem_arena* arena, u64 size, b32 non_zero) {
+
+    if (!arena || size == 0) {
+        return NULL;
+    }
+    u64 pos_aligned = ALIGN_UP_POW2(arena->current_pos, ARENA_ALIGN);
+    u64 new_pos = pos_aligned + size;
+
+    if (new_pos > arena->reserve_size) { return NULL; }
+
+    if (new_pos > arena->commit_pos) {
+        u64 new_commit_pos = new_pos;
+        new_commit_pos += arena->commit_size - 1;
+        new_commit_pos -= new_commit_pos % arena->commit_size;
+        new_commit_pos = MIN(new_commit_pos, arena->reserve_size);
+
+        u8* mem = (u8*)arena + arena->commit_pos;
+        u64 commit_size = new_commit_pos - arena->commit_pos;
+
+        if (!platform_mem_commit(mem, commit_size)) {
+            return NULL;
+        }
+
+        arena->commit_pos = new_commit_pos;
+    }
+
+    arena->current_pos = new_pos;
+
+    u8* out = (u8*)arena + pos_aligned;
+
+    if (!non_zero) {
+        memset(out, 0, size);
+    }
+
+    return out;
+}
+
+void arena_reset(mem_arena* arena) {
+    if (arena) {
+        arena->current_pos = ARENA_BASE_POS;
+    }
+}
+
+void arena_pop(mem_arena* arena, u64 size) {
+    if (!arena) {
+        return;
+    }
+    size = ALIGN_UP_POW2(size, ARENA_ALIGN);
+    size = MIN(size, arena->current_pos - ARENA_BASE_POS);
+    arena->current_pos -= size;
+}
+
+void arena_pop_to(mem_arena* arena, u64 target_pos) {
+    if (!arena) {
+        return;
+    }
+    target_pos = MAX(target_pos, ARENA_BASE_POS);
+
+    if (target_pos < arena->current_pos) {
+        arena->current_pos = target_pos;
+    }
+}
